f9570637634c88e0dc7a519057e5c0893b95a2f3
LittleLink <Simon_0_0_@live.se>
2021-03-27 01:37:09 +0100

    removed some redundant code

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 // Modules
-const request = require('sync-request'); 
-const readline = require('readline-sync');
-const chalk = require('chalk');
 const server = require('./server');
 const monitor = require('./repoMonitor'); 
 
